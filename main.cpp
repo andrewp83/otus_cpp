1decd89aae51c24bf62b50c5cbc0fb35819e6604
a.polyakov <>
2020-05-26 13:41:43 +0300

    hw_06

--- main.cpp
+++ main.cpp
@@ -1,36 +1,83 @@
 #include <iostream>
 
-#include "application.h"
-
-namespace ui {
-	void on_create() {
-		Application::instance()->get_controller().create_new_document();
-	}
-
-	void on_load_from_file(const std::string& name) {
-		Application::instance()->get_controller().open_document(name);
-	}
-
-	void on_save_to_file(const std::string& name, SaveDocumentFormat format) {
-		Application::instance()->get_controller().save_document(name, format);
-	}
-
-	void on_shape_create(ShapeType type, Point pos) {
-		Application::instance()->get_controller().create_shape(type, pos);
-	}
-
-	void on_shape_remove() {
-		Application::instance()->get_controller().remove_shape();
-	}
-
-	void select_document(size_t index) {
-		Application::instance()->get_controller().select_document(index);
-	}
+void decrypt(std::string& s, size_t beg, size_t end) {
+    size_t len = end - beg + 1;
+    if (len < 3) return;
+    size_t mid = beg + (len + 1) / 2 - 1;
+    char ch = s[beg];
+    s.insert(mid + 1, 1, ch);
+    //s.erase(mid, 1);
+    //s.insert(0, 1, ch);
+    s.erase(beg, 1);
+    decrypt(s, beg, mid - 1);
+    decrypt(s, mid + 1, end);
 }
 
 int main (int, char **) {
-    
-	Application::instance()->run();
-    
+    std::string s;
+    std::getline(std::cin, s);
+    decrypt(s, 0, s.size() - 1);
+    std::cout << s << std::endl;
     return 0;
 }
+
+//int main (int, char **) {
+//
+//	//Application::instance()->run();
+//
+//    long long int i, Fi, j, Fj, n;
+//    std::cin >> i >> Fi >> j >> Fj >> n;
+//
+//    if (j < i) {
+//        std::swap(i, j);
+//        std::swap(Fi, Fj);
+//    }
+//
+//    long long int F_i_plus_1 = 0;
+//    if (j > i + 1) {
+//        long long int x = 1, y = 1;
+//        long long int cur = i + 2;
+//        long long int x_prev;
+//        while (cur < j) {
+//            x_prev = x;
+//            x = x + y;
+//            y = x_prev;
+//            cur++;
+//        }
+//        F_i_plus_1 = (Fj - y * Fi) / x;
+//        //std::cout << x << ", " << y << std::endl;
+//    } else {
+//        F_i_plus_1 = j;
+//    }
+//
+//    long long int Fn = 0;
+//    if (n == i) {
+//        Fn = Fi;
+//    } else if (n == j) {
+//        Fn = Fj;
+//    } else if (n == i + 1) {
+//        Fn = F_i_plus_1;
+//    } else {
+//        if (n > i) {
+//            long long int cur = i + 2;
+//            while (cur <= n) {
+//                Fn = F_i_plus_1 + Fi;
+//                Fi = F_i_plus_1;
+//                F_i_plus_1 = Fn;
+//                cur++;
+//            }
+//        } else {
+//            long long int cur = i - 1;
+//            while (cur >= n) {
+//                Fn = F_i_plus_1 - Fi;
+//                F_i_plus_1 = Fi;
+//                Fi = Fn;
+//                cur--;
+//            }
+//        }
+//    }
+//
+//    std::cout << Fn << std::endl;
+//
+//    return 0;
+//}
