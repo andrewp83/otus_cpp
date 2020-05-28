2ab7f3ec09d4558759541f2084ccf80dc96f5b2f
a.polyakov <>
2020-05-28 13:22:08 +0300

    Revert "remove all files to review"
    This reverts commit 5aa304d9e1e1c3cab129cb3b3b0b8c5de6de619d.

---
+++ print_ip.hpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <limits.h>
+#include <type_traits>
+#include <tuple>
+
+static const char* DOT_SEPARATOR = ".";
+
+template<typename T>
+typename std::enable_if_t<std::is_integral<T>::value> print_ip(const T& addr, std::ostream& output = std::cout) {
+    // TO DO
+    //output << __PRETTY_FUNCTION__ << std::endl;
+    size_t num_bytes = sizeof(T);
+    size_t n = num_bytes;
+    while (n) {
+        if (n < num_bytes) {
+            output << DOT_SEPARATOR;
+        }
+        uint8_t byte = (addr >> (8 * (n - 1))) & 0xff;
+        output << static_cast<short>(byte);
+        n--;
+    }
+}
+
+// std::string
+template<typename T>
+typename std::enable_if_t<std::is_same<T, std::string>::value> print_ip(const T& addr, std::ostream& output = std::cout) {
+    output << addr;
+}
+
+// containers
+template<typename T>
+//decltype(std::begin(std::declval<T>()), std::end(std::declval<T>()), void()) print_ip(const T& addr) {
+typename std::enable_if_t< !std::is_same<T, std::string>::value
+                        && std::is_same<typename T::iterator, typename T::iterator>::value > print_ip(const T& addr, std::ostream& output = std::cout) {
+                            
+    for (auto it = addr.begin(); it != addr.end(); ++it) {
+        if (it != addr.begin()) output << DOT_SEPARATOR;
+        output << *it;
+    }
+}
+
+template<typename T, size_t N>
+struct tuple_printer : tuple_printer<T, N - 1> {
+    using base = tuple_printer<T, N - 1>;
+        
+    void print(const T& t, std::ostream& output = std::cout) {
+        static_assert(std::is_same<decltype(std::get<N - 1>(t)), decltype(std::get<N - 2>(t))>::value, "types not equal");
+        
+        base::print(t);
+        output << "." << std::get<N - 1>(t);
+    }
+};
+
+template<typename T>
+struct tuple_printer<T, 1> {
+    void print(const T& t, std::ostream& output = std::cout) {
+        output << std::get<0>(t);
+    }
+};
+
+template<typename T, size_t n = std::tuple_size<T>::value>
+decltype(std::get<0>(std::declval<T>()), void()) print_ip(const T& addr, std::ostream& output = std::cout) {
+    tuple_printer<T, n> t_pr;
+    t_pr.print(addr, output);
+}
---
+++ test_print.cpp
@@ -0,0 +1,47 @@
+#define BOOST_TEST_MODULE test_print
+
+#include "print_ip.hpp"
+
+#include <sstream>
+
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(test_print)
+
+BOOST_AUTO_TEST_CASE(test_valid_output)
+{
+    
+    std::stringstream ss;
+    print_ip(char(-1), ss);
+	BOOST_CHECK_EQUAL(ss.str(), "255");
+    
+    ss.str("");
+    print_ip(short(0), ss);
+    BOOST_CHECK_EQUAL(ss.str(), "0.0");
+    
+    ss.str("");
+    print_ip(int(2130706433), ss);
+    BOOST_CHECK_EQUAL(ss.str(), "127.0.0.1");
+    
+    ss.str("");
+    print_ip(long(8875824491850138409), ss);
+    BOOST_CHECK_EQUAL(ss.str(), "123.45.67.89.101.112.131.41");
+
+    std::string str = "24.56.78.54";
+    ss.str("");
+    print_ip(str, ss);
+    BOOST_CHECK_EQUAL(ss.str(), str);
+
+    std::vector<int> vec = {14,18,123,255};
+    ss.str("");
+    print_ip(vec, ss);
+    BOOST_CHECK_EQUAL(ss.str(), "14.18.123.255");
+
+    ss.str("");
+    std::list<int> l = {14,18,123,255};
+    print_ip(l, ss);
+    BOOST_CHECK_EQUAL(ss.str(), "14.18.123.255");
+    
+}
+
+}
