a0d59b8fbbd8de2cc2758e0bcfc372e4a8377e94
a.polyakov <>
2020-06-09 22:27:13 +0300

    Добавлена обработка некорретно введенного размера пакета

--- main.cpp
+++ main.cpp
@@ -6,6 +6,10 @@ void print_usage(std::ostream& out = std::cerr) {
     out << "Usage: bulk <commands number>" << std::endl;
 }
 
+void print_wrong_format(std::ostream& out = std::cerr) {
+    out << "Wrong number format! Usage: bulk <commands number>" << std::endl;
+}
+
 int main (int argc, char ** argv) {
     
     if (argc != 2) {
@@ -13,8 +17,15 @@ int main (int argc, char ** argv) {
         return 0; // или не  ноль?
     }
     
-    size_t bulk_size = std::stoi(argv[1]);
-
+    size_t bulk_size;
+    
+    try {
+        bulk_size = std::stoi(argv[1]);
+    } catch (...) {
+        print_wrong_format();
+        return 0; // или не  ноль?
+    }
+    
     std::unique_ptr<Application> app = std::make_unique<Application>(bulk_size);
     app->run_main_loop();
     
