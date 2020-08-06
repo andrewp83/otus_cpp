160cd317a7037e684676cd5108e66371656ff83d
a.polyakov <>
2020-08-06 21:44:19 +0300

    похоже на рабочий вариант

--- include/database.h
+++ include/database.h
@@ -7,6 +7,7 @@
 
 #pragma once
 
+#include <map>
 #include <string>
 #include <vector>
 
@@ -15,24 +16,69 @@ namespace my_db {
 
 class Database;
 
+
+class Record {
+public:
+    Record() {}
+    Record(int _id, const std::string& name) : _id(_id), name(name) {}
+    
+    size_t get_id() const { return _id; }
+    const std::string& get_name() const { return name; }
+    
+private:
+    size_t _id {0};
+    std::string name;
+};
+
+
+class Table {
+public:
+    Table(const std::string& name) : name(name) {}
+    
+    const std::vector<Record>& get_records() const { return records; }
+    
+    bool insert(const Record& record);
+    
+    void truncate();
+    
+private:
+    std::string name;
+    std::vector<Record> records;
+};
+
+
 class QueryResult {
     friend class Database;
     
 public:
     const std::vector<std::string>& get_rows() const;
     
+    void add_row(const std::string& row) { rows.push_back(row); }
+    
     bool is_ok() const;
     
     const std::string& get_error_message() const;
+    
+    void set_error_message(const std::string& msg) { error_message = msg; }
     
 private:
     std::string error_message;
     std::vector<std::string> rows;
 };
 
+
 class Database {
 public:
+    Database();
+    
     QueryResult query(const std::string& query_str);
+    
+    Table* get_table(const std::string& name);
+    
+private:
+    std::map<std::string, Table> tables;
 };
+
+std::vector<std::string> split_string(const std::string& str);
 
 }
--- include/database_sql.h
+++ include/database_sql.h
@@ -1,14 +1,88 @@
 #pragma once
 
+#include <memory>
+
+#include "database.h"
+
 namespace my_db {
 	
 class Command;
 
 using CommandPtr = std::shared_ptr<Command>;
 
+class CommandFactory {
+public:
+    static CommandPtr create_command(Database* db, const std::string& name, const std::vector<std::string>& args, std::string& error_msg);
+    
+private:
+    static CommandPtr create_insert(Database* db, const std::vector<std::string>& args, std::string& error_msg);
+    static CommandPtr create_truncate(Database* db, const std::vector<std::string>& args, std::string& error_msg);
+    static CommandPtr create_intersection(Database* db);
+    static CommandPtr create_symmetric_difference(Database* db);
+    static CommandPtr create_print(Database* db, const std::vector<std::string>& args, std::string& error_msg);
+    
+};
+
 class Command {
 public:
-	static CommandPtr create
+    Command(Database* db) : db(db) {}
+    
+    virtual QueryResult run() = 0;
+    
+    virtual ~Command() {}
+    
+protected:
+    Database* db {nullptr};
+};
+
+
+class CommandInsert : public Command {
+public:
+    CommandInsert(Database* db, const std::string& table_name, const Record& record) : Command(db), table_name(table_name), record(record) {}
+    
+    QueryResult run() override;
+    
+protected:
+    std::string table_name;
+    Record record;
+};
+
+
+class CommandTruncate : public Command {
+public:
+    CommandTruncate(Database* db, const std::string& table_name) : Command(db), table_name(table_name) {}
+    
+    QueryResult run() override;
+    
+private:
+    std::string table_name;
+};
+
+
+class CommandIntersection : public Command {
+public:
+    CommandIntersection(Database* db) : Command(db) {}
+    
+    QueryResult run() override;
+};
+
+
+class CommandSymmetricDifference : public Command {
+public:
+    CommandSymmetricDifference(Database* db) : Command(db) {}
+    
+    QueryResult run() override;
+};
+
+
+class CommandPrint : public Command {
+public:
+    CommandPrint(Database* db, const std::string& table_name) : Command(db), table_name(table_name) {}
+    
+    QueryResult run() override;
+    
+private:
+    std::string table_name;
 };
 
 
